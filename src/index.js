d0547a7b1665d63efc0511860991f4aac3aa1173
Ajay <ajaydsoni111@gmail.com>
2023-11-07 12:37:43 +0530

    added utils code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import "dotenv/config";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 console.log(process.env.PORT);
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Exprees Error : ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Your Server is listening On PORT :${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Failed :", err);
+  });
 
 /*
 import express from "express";
